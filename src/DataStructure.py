a6e0a182cf97c62281cd296eb66c0a8cb454cb98
Jasmine <jasmine880809@gmail.com>
2021-06-13 19:18:03 +0800

    [ DataStructure ] add OutputMatchPositionInfo

--- src/DataStructure.py
+++ src/DataStructure.py
@@ -4,16 +4,20 @@
 # In[1]:
 from collections import namedtuple
 
-# In[3]:
 RepeatFragNInfo = namedtuple(
     "RepeatFragNInfo", ["fragmentLenList", "count", "position"]
 )
+
+# *PositionInfo, for position in RepeatFragNInfo
 IRSPositionInfo = namedtuple(
     "IRSPositionInfo", ["chrIdx", "fragmentIdx", "baseIdx", "seq"]
-)  # for position in RepeatFragNInfo
+)
 TRSPositionInfo = namedtuple(
     "IRSPositionInfo", ["chrIdx", "fragmentIdx", "baseIdx", "seqList"]
 )
 
 RepeatEvaInfo = namedtuple("RepeatEvaInfo", ["score", "length", "mismatchRatio"])
+
 DfamConSeqInfo = namedtuple("DfamConSeqInfo", ["id", "length", "seq"])
+
+OutputMatchPositionInfo = namedtuple("OutputMatchPositionInfo", ["startIdx", "endIdx"])
