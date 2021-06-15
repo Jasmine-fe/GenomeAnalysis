5fc722b5ce74002936d7d9e133b3671acf4b72c5
Jasmine <jasmine880809@gmail.com>
2021-06-15 14:12:11 +0800

    [ DataInfo ] add currDatasetName

--- src/DataInfo.py
+++ src/DataInfo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import re
 import os
 from DataStructure import RepeatFragNInfo
 
@@ -12,12 +13,13 @@ mouseDataset = "mm39.fa"
 danioRerioDataset = "DanioRerio.fasta"
 humanDataset = "hg38.fa"
 human_chr1 = "chr1.fa"
-
+human_chrY = "chrY.fa"
 # match pattern
 humanPattern = "^chr(?:\d*|[A-Z]*)$"
 noPattern = "*"
 # shared var
 currDataset = human_chr1
+currDatasetName = re.search(r"([^.]+)", currDataset).group()
 
 datasetPath = os.path.join(os.getcwd()) + "/../dataset/" + currDataset
 matchPattern = noPattern
