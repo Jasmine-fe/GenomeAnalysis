922e437d5abe597a88a5593b97762b4c17b289ec
Jasmine <jasmine880809@gmail.com>
2021-06-18 19:52:47 +0800

    [ RepeatEvaluation ] check output match accuracy

--- src/Evaluation/RepeatEvaluation.py
+++ src/Evaluation/RepeatEvaluation.py
@@ -3,32 +3,40 @@
 
 # In[1]:
 import re
+import os
 import math
 import numpy as np
 import pandas as pd
 from collections import namedtuple
-from DataStructure import DfamConSeqInfo, OutputMatchPositionInfo
+from DataStructure import DfamConSeqInfo, PositionInfo
 from DataInfo import (
     cutterLen,
     fragmentN,
 )
 
-# In[162]:
+"""
+Params
+positionList: [(startIdx, endIdx), ...]
+"""
 
 
-def getConsensusSeqIds():
-    dfamHitDataset = pd.read_csv(
-        "Evaluation/Source/hg38_dfam_nrph_chr1.hits", sep="\t", header=0
+def positionBucketClassifier(positionList, bucketNum=10):
+    positionLookupDic = {}
+    eachBucketNum = int(len(positionList) / bucketNum)
+    for i in range(bucketNum):
+        firstStartIdx, lastStartIdx = (
+            positionList[eachBucketNum * i].startIdx,
+            positionList[(eachBucketNum * (i + 1) - 1)].startIdx,
+        )
+        positionLookupDic[i] = (firstStartIdx, lastStartIdx)
+    # handle eachBucketNum remainder
+    positionLookupDic[bucketNum - 1] = (
+        positionLookupDic[bucketNum - 1][0],
+        positionList[-1].startIdx,
     )
-
-    familyAcIds = list(set(dfamHitDataset["family_acc"]))
-    familyAcIds = [re.search("DF\d*", i).group() for i in familyAcIds]
-    familyAcIds = sorted(familyAcIds)
-    familyAcIdsDf = pd.DataFrame(familyAcIds, columns=["id"])
-    return familyAcIdsDf
+    return positionLookupDic
 
 
-# In[165]:
 def getDfamConSeqData():
     # init DfamConSeqInfo value
     dfId = ""
@@ -64,13 +72,13 @@ def getDfamConSeqData():
 
 
 # In[ ]:
-def getOutputMatchPosition(repeatInfoList):
+def getRepeatPositionList(repeatInfoList):
     matchList = []
     for repeatN in repeatInfoList:
         repeatFragNLen = sum(repeatN.fragmentLenList) + (cutterLen * fragmentN)
         for fragposition in repeatN.position:
             matchList.append(
-                OutputMatchPositionInfo(
+                PositionInfo(
                     fragposition.baseIdx, fragposition.baseIdx + repeatFragNLen
                 )
             )
@@ -78,70 +86,102 @@ def getOutputMatchPosition(repeatInfoList):
     return matchList
 
 
-# In[ ]:
-"""
-Params
-outputPositionList: [ OutputMatchPositionInfo(startIdx, endIdx), ...]
-"""
-
-
-def outputPositionBucketClassifier(outputPositionList, bucketNum=10):
-    outputPositionLookupDic = {}
-    eachBucketNum = int(len(outputPositionList) / bucketNum)
-    for i in range(bucketNum):
-        firstStartIdx, lastStartIdx = (
-            outputPositionList[eachBucketNum * i].startIdx,
-            outputPositionList[(eachBucketNum * (i + 1) - 1)].startIdx,
-        )
-        outputPositionLookupDic[i] = (firstStartIdx, lastStartIdx)
-    outputPositionLookupDic[bucketNum - 1] = (
-        outputPositionLookupDic[bucketNum - 1][0],
-        outputPositionList[-1].startIdx,
-    )  # handle eachBucketNum remainder
-    return outputPositionLookupDic
-
-
-# In[ ]:
-def checkDfamMatch(
-    outputPositionLookupDic,
-    OutputMatchPositionList,
-    DfamFileName="hg38_dfam_nrph_chr1",
-    bucketNum=10,
+def generateDfamPositionData(
+    readFileName,
+    outputFileName,
 ):
     dfamDataset = pd.read_csv(
-        f"Evaluation/Source/{DfamFileName}.hits",
+        f"Evaluation/Source/{readFileName}.hits",
         sep="\t",
         header=0,
         usecols=["familyAcc", "familyName", "aliSt", "aliEn", "envSt", "envEn"],
     )
-    dfamDatasetMatchList = [False] * len(dfamDataset)
-    eachBucketNum = int(len(OutputMatchPositionList) / bucketNum)
+    ouputFilePath = f"Evaluation/Source/{outputFileName}.txt"
+    outputCols = ["familyAcc", "familyName", "startIdx", "endIdx"]
+    with open(ouputFilePath, "w") as outputFile:
+        outputFile.write("\t".join(outputCols) + "\n")
+        for idx in range(len(dfamDataset)):
+            row = dfamDataset.iloc[idx]
+            li = sorted([row.aliSt, row.aliEn, row.envSt, row.envEn])
+            start, end = li[0], li[-1]
+            output = f"{row.familyAcc}\t{row.familyName}\t{start}\t{end}\n"
+            outputFile.write(output)
+
+
+def getDfamPositionList(readFileName):
+    dfamDataset = pd.read_csv(
+        f"Evaluation/Source/{readFileName}.txt",
+        sep="\t",
+        header=0,
+        usecols=["startIdx", "endIdx"],
+    )
+    positionList = []
     for idx in range(len(dfamDataset)):
         row = dfamDataset.iloc[idx]
-        li = sorted([row.aliSt, row.aliEn, row.envSt, row.envEn])
-        start = li[0]
-        end = li[-1]
-        length = end - start
+        positionList.append(PositionInfo(row.startIdx, row.endIdx))
+    positionList.sort(key=lambda tup: tup[0])
+    return positionList
+
+
+# In[ ]:
+def checkDfamMatch(
+    outputPositionLookupDic, outputMatchPositionList, readFileName, bucketNum=10
+):
+    dfamDataset = pd.read_csv(
+        f"Evaluation/Source/{readFileName}.txt",
+        sep="\t",
+        header=0,
+        usecols=["familyAcc", "familyName", "startIdx", "endIdx"],
+    )
+    dfamDatasetMatchList = [False] * len(dfamDataset)
+    eachBucketNum = int(len(outputMatchPositionList) / bucketNum)
+    for idx in range(len(dfamDataset)):
+        row = dfamDataset.iloc[idx]
+        start, end = row.start, row.end
         flag = 0
         for bucketIdx in range(bucketNum):
             if (
                 start >= outputPositionLookupDic[bucketIdx][0]
                 and start <= outputPositionLookupDic[bucketIdx][1]
             ):
-                for outputRow in OutputMatchPositionList[
+                for outputRow in outputMatchPositionList[
                     eachBucketNum * bucketIdx : eachBucketNum * (bucketIdx + 1) - 1
                 ]:
-                    if (
-                        (outputRow.startIdx >= start and outputRow.startIdx <= end)
-                        or (outputRow.endIdx <= start and outputRow.endIdx >= end)
-                        or (
-                            (outputRow.startIdx + outputRow.endIdx) / 2 >= start
-                            and (outputRow.startIdx + outputRow.endIdx) / 2 <= end
-                        )
-                    ):
+                    if outputRow.startIdx in range(
+                        start, end
+                    ) or outputRow.endIdx in range(start, end):
                         flag = 1
                         dfamDatasetMatchList[idx] = True
                         break
-                if flag:
-                    break
+            if flag:
+                break
     return dfamDatasetMatchList
+
+
+# In[ ]:
+# check output position with TE in Dfam
+def checkOutputMatch(
+    outputMatchPositionList, dfamPositionList, dfamPositionLookupDic, bucketNum=10
+):
+    outputMatchList = [False] * len(outputMatchPositionList)
+    eachBucketNum = int(len(dfamPositionList) / bucketNum)
+    for idx, outputPosition in enumerate(outputMatchPositionList):
+        start, end = outputPosition[0], outputPosition[1]
+        flag = 0
+        for bucketIdx in range(bucketNum):
+            if (
+                start >= dfamPositionLookupDic[bucketIdx][0]
+                and start <= dfamPositionLookupDic[bucketIdx][1]
+            ):
+                for dfamPosition in dfamPositionList[
+                    eachBucketNum * bucketIdx : eachBucketNum * (bucketIdx + 1) - 1
+                ]:
+                    if dfamPosition.startIdx in range(
+                        start, end
+                    ) or dfamPosition.endIdx in range(start, end):
+                        flag = 1
+                        outputMatchList[idx] = True
+                        break
+            if flag == 1:
+                break
+    return outputMatchList
