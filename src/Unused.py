72843401267749d416957b592274ec5160d945bf
Jasmine <jasmine880809@gmail.com>
2021-07-11 14:58:22 +0800

    [ Unused ] code from main

---
+++ src/Unused.py
@@ -0,0 +1,4 @@
+# # tandem Repeat in N fragment
+# tandemRepeatLenList = list(filter(checkTandemRepeatExist, repeatFragNLenList))
+# tandemRepeatInfoList = integrateRepeatInfo(fragmentsSeqList, fragmentsLenList, tandemRepeatLenList, repeatFragNPositionDict, repeatType=1)
+# generateTROutputFile(tandemRepeatInfoList, outputFileName= f"{currDatasetName}TRS", matchRatioOfSum=0.4)
