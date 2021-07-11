0e0ba5c8e365b4a31b71817d320246da4b574b69
Jasmine <jasmine880809@gmail.com>
2021-07-11 09:25:07 +0800

    [ RepeatClassification ] printFamilyNameTable

--- src/Evaluation/RepeatClassification.py
+++ src/Evaluation/RepeatClassification.py
@@ -1,3 +1,12 @@
+from prettytable import PrettyTable
+
+
 class RepeatClassification:
     def __init__(self):
         pass
+
+    def printFamilyNameTable(self, familyNameCounter):
+        table = PrettyTable(["FamilyName", "Count"])
+        for i in list(familyNameCounter):
+            table.add_row([i[0], i[1]])
+        print(table)
