e8298a36e8897f4d9856e532b03d0fbe2d522df3
Jasmine <jasmine880809@gmail.com>
2021-07-24 13:00:50 +0800

    [ DfamEva ] DfamEva inherit RepeatEvaluation

--- src/Evaluation/DfamEvaluation.py
+++ src/Evaluation/DfamEvaluation.py
@@ -3,7 +3,8 @@ import math
 import numpy as np
 import pandas as pd
 from Bio import SeqIO, pairwise2
-from DataInfo import chrPattern, chrKey
+from DataInfo import chrPattern, humanChrKey
+from Evaluation.RepeatEvaluation import RepeatEvaluation
 from DataStructure import (
     DfamConSeqInfo,
     PositionInfo,
@@ -12,83 +13,30 @@ from DataStructure import (
 )
 
 
-class DfamEvaluation:
-    def __init__(self):
+class DfamEvaluation(RepeatEvaluation):
+    def __init__(self, repeatInfoList):
+        super().__init__(repeatInfoList)
+        hitFileName = "chrX_dm6_dfam.nrph.hits"
         self.dfamPositionList = []
         self.dfamPositionLookupDic = dict()
-
-    def getDfamConSeqData(self):
-        # init DfamConSeqInfo value
-        dfId = ""
-        seqLineCount = 1
-        currLineCount = 0
-        seqLength = 0
-        seq = ""
-        seqFlag = 0
-
-        DfamRefDataset = open("Evaluation/Source/Dfam.embl.txt", "r")
-        lines = DfamRefDataset.readlines()
-        DfamRefSeqList = []
-        for eachLine in lines:
-            if re.search("^AC", eachLine):
-                dfId = re.search("DF\d*", eachLine).group()
-            elif re.search("^SQ", eachLine):
-                seqFlag = 1
-                currLineCount = 0
-                seqLineCount = math.ceil(seqLength / 60)
-                seqLength = int(re.search("\d+", eachLine).group())
-            elif seqFlag == 1 and (currLineCount <= seqLineCount):
-                matchList = re.findall("[a-zA-Z]+", eachLine)
-                segmentSeq = "".join(matchList)
-                seq += segmentSeq
-                currLineCount += 1
-                if currLineCount >= seqLineCount:
-                    DfamRefSeqList.append(
-                        DfamConSeqInfo(id=dfId, length=seqLength, seq=seq)
-                    )
-                    seq = ""
-                    seqFlag = 0
-        return DfamRefSeqList
-
-    def generateDfamPositionData(
-        self,
-        readFileName,
-        outputFileName,
-    ):
-        dfamDataset = pd.read_csv(
-            f"Evaluation/Source/{readFileName}.hits",
-            sep="\t",
-            header=0,
-            usecols=["familyAcc", "familyName", "aliSt", "aliEn", "envSt", "envEn"],
+        self.familyPositionList = []
+        self.familySeqSimilarityList = []
+        self.dfamBucketAmount = 10
+        self.dfamEachBucketNum = 0
+        self.dfamHitData = pd.read_csv(
+            f"Evaluation/Source/{hitFileName}", sep="\t", header=0
         )
-        ouputFilePath = f"Evaluation/Source/{outputFileName}.txt"
-        outputCols = ["familyAcc", "familyName", "startIdx", "endIdx"]
-        with open(ouputFilePath, "w") as outputFile:
-            outputFile.write("\t".join(outputCols) + "\n")
-            for idx in range(len(dfamDataset)):
-                row = dfamDataset.iloc[idx]
-                li = sorted([row.aliSt, row.aliEn, row.envSt, row.envEn])
-                start, end = li[0], li[-1]
-                output = f"{row.familyAcc}\t{row.familyName}\t{start}\t{end}\n"
-                outputFile.write(output)
 
-    def getDfamPositionList(self, readFileName):
-        dfamDataset = pd.read_csv(
-            f"Evaluation/Source/{readFileName}",
-            sep="\t",
-            header=0,
-            usecols=[
-                "familyAcc",
-                "familyName",
-                "startIdx",
-                "endIdx",
-            ],
-        )
-        for idx in range(len(dfamDataset)):
-            row = dfamDataset.iloc[idx]
+    def getDfamPositionList(self):
+        """
+        get DfamPositionInfo format data for .hit file
+        """
+        for idx, refRow in self.dfamHitData.iterrows():
+            startIdx = refRow["ali-st"] if refRow["strand"] == "+" else refRow["ali-en"]
+            endIdx = refRow["ali-en"] if refRow["strand"] == "+" else refRow["ali-st"]
             self.dfamPositionList.append(
                 DfamPositionInfo(
-                    row.familyAcc, row.familyName, row.startIdx, row.endIdx
+                    refRow["family_acc"], refRow["family_name"], startIdx, endIdx
                 )
             )
         self.dfamPositionList.sort(
@@ -96,107 +44,78 @@ class DfamEvaluation:
         )  # choose by key name (startIdx)
         return self.dfamPositionList
 
-    def positionBucketClassifier(self, bucketNum=10):
-        eachBucketNum = int(len(self.dfamPositionList) / bucketNum)
-        for i in range(bucketNum):
-            firstStartIdx, lastStartIdx = (
-                self.dfamPositionList[eachBucketNum * i].startIdx,
-                self.dfamPositionList[(eachBucketNum * (i + 1) - 1)].startIdx,
-            )
-            self.dfamPositionLookupDic[i] = (firstStartIdx, lastStartIdx)
-        # handle eachBucketNum remainder
-        self.dfamPositionLookupDic[bucketNum - 1] = (
-            self.dfamPositionLookupDic[bucketNum - 1][0],
-            self.dfamPositionList[-1].startIdx,
-        )
-        return self.dfamPositionLookupDic
+    # def dfamPositionBucketClassifier(self, bucketNum=10):
+    #     self.dfamEachBucketNum = int(len(self.dfamPositionList) / bucketNum)
+    #     for i in range(bucketNum):
+    #         firstStartIdx, lastStartIdx = (
+    #             self.dfamPositionList[self.dfamEachBucketNum * i].startIdx,
+    #             self.dfamPositionList[(self.dfamEachBucketNum * (i + 1) - 1)].startIdx,
+    #         )
+    #         self.dfamPositionLookupDic[i] = (firstStartIdx, lastStartIdx)
+    #     # handle eachBucketNum remainder
+    #     self.dfamPositionLookupDic[bucketNum - 1] = (
+    #         self.dfamPositionLookupDic[bucketNum - 1][0],
+    #         self.dfamPositionList[-1].startIdx,
+    #     )
+    #     return self.dfamPositionLookupDic
 
-    def checkDfamMatch(
-        self,
-        repeatPositionList,
-        repeatPositionLookupDic,
-        readFileName,
-        bucketNum=10,
-    ):
-        dfamDataset = pd.read_csv(
-            f"Evaluation/Source/{readFileName}.txt",
-            sep="\t",
-            header=0,
-            usecols=["familyAcc", "familyName", "startIdx", "endIdx"],
-        )
-        dfamDatasetMatchList = [False] * len(dfamDataset)
-        eachBucketNum = int(len(repeatPositionList) / bucketNum)
-        for idx in range(len(dfamDataset)):
-            row = dfamDataset.iloc[idx]
-            start, end = row.start, row.end
+    # check other match than 0, 1
+    def checkRepeatMatchWithDfam(self):
+        repeatMatchList = [False] * len(self.repeatPositionList)
+        matchedFamilyAccList, matchedFamilyNameList = [], []
+        for idx, repeatPosition in enumerate(self.repeatPositionList):
+            repeatStart, repeatEnd = repeatPosition[0], repeatPosition[1]
             flag = 0
-            for bucketIdx in range(bucketNum):
+            for bucketIdx in range(self.bucketAmount):
                 if (
-                    start >= repeatPositionLookupDic[bucketIdx][0]
-                    and start <= repeatPositionLookupDic[bucketIdx][1]
+                    self.repeatPositionLookupDic[bucketIdx][0]
+                    <= repeatStart
+                    <= self.repeatPositionLookupDic[bucketIdx][1]
                 ):
-                    for outputRow in repeatPositionList[
-                        eachBucketNum * bucketIdx : eachBucketNum * (bucketIdx + 1) - 1
-                    ]:
-                        if outputRow.startIdx in range(
-                            start, end
-                        ) or outputRow.endIdx in range(start, end):
+                    for ref in self.dfamPositionList:
+                        if repeatStart in range(
+                            ref.startIdx, ref.endIdx
+                        ) or repeatEnd in range(ref.startIdx, ref.endIdx):
+                            matchedFamilyAccList.append(ref.familyAcc)
+                            matchedFamilyNameList.append(ref.familyName)
                             flag = 1
-                            dfamDatasetMatchList[idx] = True
+                            repeatMatchList[idx] = True
                             break
-                if flag:
+                if flag == 1:
                     break
-        return dfamDatasetMatchList
+        return repeatMatchList, matchedFamilyAccList, matchedFamilyNameList
 
+    # ----------- For specific family --------------------------------------
 
-def familyHitChrGroupBy(familyHitFile):
-    df = pd.read_csv(f"./Evaluation/Source/{familyHitFile}", delimiter="\t")
-    chrClassification = df.groupby("seqName")
-    chrClassification.size()
-    chrFamilyCount = {}
-    for index, value in chrClassification.size().items():
-        if re.match(chrPattern, index):
-            chrFamilyCount[index] = value
-    return chrFamilyCount
-
-
-def getfamilySeq(familyHitFile, seq):
-    familyPosition = pd.read_csv(
-        f"Evaluation/Source/{familyHitFile}",
-        sep="\t",
-        header=0,
-        usecols=["familyAcc", "familyName", "startIdx", "endIdx"],
-    )
-    familySeqList = []
-    for idx, row in familyPosition.iterrows():
-        familySeqList.append(seq[row["startIdx"] : row["endIdx"]])
-    return familySeqList
-
-
-def getConsensusSeq(consensusSeqFile):
-    consensusSeqPath = f"./Evaluation/Source/{consensusSeqFile}"
-    fastaSeqs = SeqIO.parse(open(consensusSeqPath), "fasta")
-    seqs = [i.seq for i in fastaSeqs]
-    consensusSeq = seqs[0]
-    return consensusSeq
-
-
-def refSeqSimilarity(consensusSeq, familySeqList):
-    familySeqSimilarityList = []
-    for idx, targetSeq in enumerate(familySeqList):
-        alignments = pairwise2.align.localxx(consensusSeq, targetSeq)
-        targetLength = len(targetSeq)
-        similarityPercentage = (
-            round(alignments[0].score / targetLength, 2) if len(alignments) > 0 else 0
+    def getfamilyPositionList(self, familyName):
+        familyPositionIterator = filter(
+            lambda x: x["familyAcc"] == familyName, self.dfamPositionList
         )
-        familySeqSimilarityList.append(
-            refSeqSimilarityInfo(
-                hitId=idx,
-                targetSeqLength=targetLength,
-                similarityPercentage=similarityPercentage,
+        self.familyPositionList = list(familyPositionIterator)
+        return self.familyPositionList
+
+    def getfamilySeqList(self, familyName, seq):
+        self.familySeqList = []
+        familyPositionList = self.getfamilyPositionList(familyName)
+        for row in familyPositionList:
+            self.familySeqList.append(seq[row["startIdx"] : row["endIdx"]])
+        return self.familySeqList
+
+    def refSeqSimilarity(self, consensusSeq, familySeqList):
+        familySeqSimilarityList = []
+        for idx, targetSeq in enumerate(familySeqList):
+            alignments = pairwise2.align.localxx(consensusSeq, targetSeq)
+            targetLength = len(targetSeq)
+            similarityPercentage = (
+                round(alignments[0].score / targetLength, 2)
+                if len(alignments) > 0
+                else 0
             )
-        )
-        print(
-            f"hitId: {idx}, targetLength: {targetLength}, similarityPercentage: {similarityPercentage}"
-        )
-    return familySeqSimilarityList
+            familySeqSimilarityList.append(
+                refSeqSimilarityInfo(
+                    hitId=idx,
+                    targetSeqLength=targetLength,
+                    similarityPercentage=similarityPercentage,
+                )
+            )
+        return familySeqSimilarityList
--- src/Evaluation/RepeatEvaluation.py
+++ src/Evaluation/RepeatEvaluation.py
@@ -7,8 +7,9 @@ import os
 import math
 import numpy as np
 import pandas as pd
+from Bio import pairwise2
 from collections import namedtuple
-from DataStructure import DfamConSeqInfo, PositionInfo, DfamPositionInfo
+from DataStructure import DfamConSeqInfo, PositionInfo
 from DataInfo import (
     cutterLen,
     fragmentN,
@@ -20,6 +21,8 @@ class RepeatEvaluation:
         self.repeatInfoList = repeatInfoList
         self.repeatPositionList = []
         self.repeatPositionLookupDic = dict()
+        self.bucketAmount = 10
+        self.eachBucketNum = 0
 
     def getRepeatPositionList(self):
         for repeatN in self.repeatInfoList:
@@ -38,46 +41,74 @@ class RepeatEvaluation:
     positionList: [(startIdx, endIdx), ...]
     """
 
-    def positionBucketClassifier(self, bucketNum=10):
-        eachBucketNum = int(len(self.repeatPositionList) / bucketNum)
-        for i in range(bucketNum):
+    def positionBucketClassifier(self):
+        self.eachBucketNum = int(len(self.repeatPositionList) / self.bucketAmount)
+        for i in range(self.bucketAmount):
             firstStartIdx, lastStartIdx = (
-                self.repeatPositionList[eachBucketNum * i].startIdx,
-                self.repeatPositionList[(eachBucketNum * (i + 1) - 1)].startIdx,
+                self.repeatPositionList[self.eachBucketNum * i].startIdx,
+                self.repeatPositionList[(self.eachBucketNum * (i + 1) - 1)].startIdx,
             )
             self.repeatPositionLookupDic[i] = (firstStartIdx, lastStartIdx)
         # handle eachBucketNum remainder
-        self.repeatPositionLookupDic[bucketNum - 1] = (
-            self.repeatPositionLookupDic[bucketNum - 1][0],
+        self.repeatPositionLookupDic[self.bucketAmount - 1] = (
+            self.repeatPositionLookupDic[self.bucketAmount - 1][0],
             self.repeatPositionList[-1].startIdx,
         )
         return self.repeatPositionLookupDic
 
     # check output position with TE in Dfam
-    def checkOutputMatch(self, dfamPositionList, dfamPositionLookupDic, bucketNum=10):
-        outputMatchList = [False] * len(self.repeatPositionList)
-        eachBucketNum = int(len(dfamPositionList) / bucketNum)
-        matchedFamilyAccList, matchedFamilyNameList = [], []
-        for idx, repeatPosition in enumerate(self.repeatPositionList):
-            start, end = repeatPosition[0], repeatPosition[1]
-            flag = 0
-            for bucketIdx in range(bucketNum):
-                if (
-                    dfamPositionLookupDic[bucketIdx][0]
-                    <= start
-                    <= dfamPositionLookupDic[bucketIdx][1]
-                ):
-                    for dfamPosition in dfamPositionList[
-                        eachBucketNum * bucketIdx : eachBucketNum * (bucketIdx + 1) - 1
-                    ]:
-                        if dfamPosition.startIdx in range(
-                            start, end
-                        ) or dfamPosition.endIdx in range(start, end):
-                            matchedFamilyAccList.append(dfamPosition.familyAcc)
-                            matchedFamilyNameList.append(dfamPosition.familyName)
-                            flag = 1
-                            outputMatchList[idx] = True
-                            break
-                if flag == 1:
-                    break
-        return outputMatchList, matchedFamilyAccList, matchedFamilyNameList
+    # def checkOutputMatch(self, dfamPositionList, dfamPositionLookupDic, bucketNum=10):
+    #     outputMatchList = [False] * len(self.repeatPositionList)
+    #     matchedFamilyAccList, matchedFamilyNameList = [], []
+    #     for idx, repeatPosition in enumerate(self.repeatPositionList):
+    #         start, end = repeatPosition[0], repeatPosition[1]
+    #         flag = 0
+    #         for bucketIdx in range(bucketNum):
+    #             if (
+    #                 dfamPositionLookupDic[bucketIdx][0]
+    #                 <= start
+    #                 <= dfamPositionLookupDic[bucketIdx][1]
+    #             ):
+    #                 for dfamPosition in dfamPositionList[
+    #                     (self.eachBucketNum * bucketIdx) : (
+    #                         self.eachBucketNum * (bucketIdx + 1) - 1
+    #                     )
+    #                 ]:
+    #                     if dfamPosition.startIdx in range(
+    #                         start, end
+    #                     ) or dfamPosition.endIdx in range(start, end):
+    #                         matchedFamilyAccList.append(dfamPosition.familyAcc)
+    #                         matchedFamilyNameList.append(dfamPosition.familyName)
+    #                         flag = 1
+    #                         print("Match")
+    #                         outputMatchList[idx] = True
+    #                         break
+    #             if flag == 1:
+    #                 break
+    #     return outputMatchList, matchedFamilyAccList, matchedFamilyNameList
+
+
+# ---------------------------- For single result ---------------------------------
+
+
+def printPairSeq(repeatInfoListEle):
+    print("FragmentN: ", fragmentN)
+    print("FragmentLenList: ", repeatInfoListEle.fragmentLenList)
+    print("Count: ", repeatInfoListEle.count)
+    print("Position: ")
+    for idx, row in enumerate(repeatInfoListEle.position):
+        print(f"{idx} - baseIdx:{row.baseIdx} , seq:{row.seq}")
+
+
+def getAlignResult(repeatInfoListEle):
+    alignments = pairwise2.align.localxx(
+        repeatInfoListEle.position[0].seq, repeatInfoListEle.position[1].seq
+    )
+    print(
+        "Alignment Result\t",
+        f"length:{(alignments[0].end - alignments[0].start)}\t",
+        "score:",
+        alignments[0].score,
+    )
+    print("seqA:", alignments[0].seqA)
+    print("seqA:", alignments[0].seqB)
