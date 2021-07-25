bcd77e63596ce2cf19e84e10160915cabf0f1396
Jasmine <jasmine880809@gmail.com>
2021-07-25 15:25:11 +0800

    [ Unused ] add unused code from main

--- src/Unused.py
+++ src/Unused.py
@@ -2,3 +2,22 @@
 # tandemRepeatLenList = list(filter(checkTandemRepeatExist, repeatFragNLenList))
 # tandemRepeatInfoList = integrateRepeatInfo(fragmentsSeqList, fragmentsLenList, tandemRepeatLenList, repeatFragNPositionDict, repeatType=1)
 # generateTROutputFile(tandemRepeatInfoList, outputFileName= f"{currDatasetName}TRS", matchRatioOfSum=0.4)
+
+# interspersed repetitive sequences
+# repeatInfoList = integrateRepeatInfo(fragmentsSeqList, fragmentsLenList, repeatFragNLenList, repeatFragNPositionDict, repeatType=2)
+# filterRepeatInfoList = filterRepeatInfo(repeatInfoList)
+# seqPermutation = getIRComb(repeatInfoList)
+# repeatEvaInfoList = generateIROutputFile(seqPermutation, outputFileName= f"{currDatasetName}_IRS", matchRatioOfSum=0.8)
+
+# evaluation = RepeatEvaluation(repeatInfoList)
+# repeatPositionList = evaluation.getRepeatPositionList()
+# repeatPositionLookupDic = evaluation.positionBucketClassifier()
+# outputMatchList, matchedFamilyAccList, matchedFamilyNameList = evaluation.checkOutputMatch(dfamPositionList, dfamPositionLookupDic, bucketNum= 10)
+
+# fragmentsLenList, fragmentsSeqList = parseSeqByCutter(parseFastaSeqs)
+# repeatFragNLenList, repeatFragNPositionDict = findRepeatSeqs(fragmentsLenList)
+# interspersed repetitive sequences
+# repeatInfoList = integrateRepeatInfo(fragmentsSeqList, fragmentsLenList, repeatFragNLenList, repeatFragNPositionDict, repeatType=2)
+# filterRepeatInfoList = filterRepeatInfo(repeatInfoList)
+# seqPermutation = getIRComb(repeatInfoList)
+# repeatEvaInfoList = generateIROutputFile(seqPermutation, outputFileName= f"{currDatasetName}_IRS", matchRatioOfSum=0.8)
