3f38d3437c4c7e9fdfde3c89423d712cb82a2907
Jasmine <jasmine880809@gmail.com>
2021-08-07 11:39:04 +0800

    [ Main ] fine tune

--- src/Unused.py
+++ src/Unused.py
@@ -27,3 +27,32 @@
 #     seq = str(parseFastaA[0][row.startIdx: row.endIdx])
 #     seqLength = row.endIdx - row.startIdx
 #     ROO_LTR_df = ROO_LTR_df.append({'seq': seq, 'seqLength': seqLength}, ignore_index=True)
+
+# Similarity plot
+# def consensusSeqSimilarity(consensusSeq, seqDf):
+#     print("hihi", len(seqDf))
+#     seqSimilarityList = []
+#     for targetSeq in seqDf:
+#         alignments = pairwise2.align.localxx(consensusSeq, targetSeq)
+#         targetLength = len(targetSeq)
+#         similarityPercentage = round(alignments[0].score / targetLength, 2)
+#         seqSimilarityList.append(similarityPercentage)
+#     return seqSimilarityList
+
+# xData = [*range(len(seqSimilarityList))]
+# fig, ax = plt.subplots(figsize=(12, 5), dpi=300)
+# sns.set_style("whitegrid")
+# sns.stripplot(x=xData, y=seqSimilarityList, linewidth=1.0, color=colorA)
+# ax.set_ylabel("Similarity", size=15)
+# ax.set_ylim(0,1.2)
+
+# ROO_LTR_df = pd.read_csv("./Evaluation/Source/ROO_LTR_Seq.csv")
+# conParseFasta = parseFasta(
+#     "DF0001696_ROO_LTR",
+#     "./Evaluation/Source/DF0001696_ROO_LTR.fa",
+#     "*",
+#     matchMode=False,
+# )
+# consensusSeq = conParseFasta[0].upper()
+# seqDf = ROO_LTR_df["seq"]
+# seqSimilarityList = consensusSeqSimilarity(consensusSeq, seqDf)
